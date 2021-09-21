9ea91388e4dacc687ea6c078985b65bd0b7bdfaf
asfand687 <asfandyar687@gmail.com>
2021-09-22 00:13:15 +0500

    added the hero content and encode sans google font

--- src/components/HeroSection/HeroElements.jsx
+++ src/components/HeroSection/HeroElements.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { MdArrowForward, MdKeyboardArrowRight } from 'react-icons/md'
 
 export const HeroContainer = styled.div`
 	background: #0c0c0c;
@@ -9,6 +10,17 @@ export const HeroContainer = styled.div`
 	height: 800px;
 	position: relative;
 	z-index: 1;
+
+	&::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+		background: rgba(0, 0, 0, 0.3);
+		z-index: 2;
+	}
 `
 
 export const HeroBg = styled.div`
@@ -28,4 +40,46 @@ export const VideoBg = styled.video`
 	-o-object-fit: cover;
 	object-fit: cover;
 	background: #232a34;
+`
+
+export const HeroContent = styled.div`
+	z-index: 3;
+	max-width: 1200px;
+	position: absolute;
+	padding: 8px 24px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const HeroH1 = styled.h1`
+	color: #fff;
+	font-size: clamp(32px, 5vw + 5px, 48px);
+	text-align: center;
+`
+
+export const HeroP = styled.p`
+	margin-top: 24px;
+	color: #fff;
+	font-size: 24px;
+	text-align: center;
+	max-width: 600px;
+	font-size: clamp(18px, 2vw + 5px, 24px);
+`
+
+export const HeroBtnWrapper = styled.div`
+	margin-top: 32px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`
+
+export const ArrowForward = styled(MdArrowForward)`
+	margin-left: 8px;
+	font-size: 20px;
+`
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+	margin-left: 8px;
+	font-size: 20px;
 `
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
+import { Button } from '../ButtonElements'
 import Video from '../../videos/video.mp4'
-import { HeroBg, HeroContainer, VideoBg } from './HeroElements'
+import {
+	ArrowForward,
+	ArrowRight,
+	HeroBg,
+	HeroBtnWrapper,
+	HeroContainer,
+	HeroContent,
+	HeroH1,
+	HeroP,
+	VideoBg,
+} from './HeroElements'
 
 const HeroSection = () => {
+	const [hover, setHover] = useState(false)
+
+	const onHover = () => {
+		setHover(!hover)
+	}
+
 	return (
 		<HeroContainer id='home'>
 			<HeroBg>
 				<VideoBg autoPlay loop muted src={Video} type='video/mp4' />
 			</HeroBg>
+			<HeroContent>
+				<HeroH1>Virtual Banking Made Easy</HeroH1>
+				<HeroP>
+					Sign up for a new account and receive $250 in next payment.
+				</HeroP>
+				<HeroBtnWrapper>
+					<Button
+						to='signup'
+						onMouseEnter={onHover}
+						onMouseLeave={onHover}
+						primary
+						dark
+					>
+						Get started {hover ? <ArrowForward /> : <ArrowRight />}
+					</Button>
+				</HeroBtnWrapper>
+			</HeroContent>
 		</HeroContainer>
 	)
 }
